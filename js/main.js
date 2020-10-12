53907ed92e08c700d9dddbd707d7a3b1ad25a1d2
Roman Nasyrov <romnasi@gmail.com>
2020-10-13 02:50:23 +0300

    Добавляет функционал заполнения и отображения карточки первого объявления из массива с данными

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 'use strict';
 
 const Offer = {
+  TITLE: [
+    `Изящество классики,  уют прованса`,
+    `Надежный приют`,
+    `Залог покоя и надежности`,
+    `Уголок киномана`,
+    `Симфония стиля`,
+    `Семейные традиции`,
+    `Симбиоз авангарда и классики`,
+    `Гармония, построенная на принципах свободы`
+  ],
   TYPE: [`palace`, `flat`, `house`, `bungalow`],
   CHECKIN: [`12:00`, `13:00`, `14:00`],
   CHECKOUT: [`12:00`, `13:00`, `14:00`],
   FEATURES: [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`],
+  DESCRIPTION: [
+    `Рядом с домом парк отличное место для отдыха и пеших прогулок`,
+    `Улучшенная планировка и большая площадь. 44 кв.м. общей площади и 9 метровая кухня это гораздо больше, чем в стандартной 1-комнатной квартире.`,
+    `Дополнительная площадь к жилому пространству в виде двух лоджий 5 кв.м.`,
+    `Удобная геометрия квартиры. Благодаря алькову расположенному в комнате можно выделить спальную зону или установить большой шкаф-купе без ущерба функционалу жилого пространства.`,
+    `Все плюсы и удобства раздельного санузла.`,
+    `Можно дышать свежим воздухом не вдыхая смог проезжающего автотранспорта благодаря тому, что окна квартиры выходят на парк.`,
+    `Отсутствует проблема с парковкой. Всегда свободные парковочные места, можно удобно припарковаться рядом с домом.`,
+    `Возможность въехать в квартиру на следующий день после сделки.`
+  ],
   MIN_FEATURES: 0,
   PHOTOS: [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`],
   MIN_PHOTOS: 1,
-  MIN_PRICE: 100,
-  MAX_PRICE: 1000,
+  MIN_PRICE: 500,
+  MAX_PRICE: 5000,
   MIN_ROOMS: 1,
   MAX_ROOMS: 12,
   PERSON_PER_ROOM: 2
@@ -29,11 +49,25 @@ const Pin = {
 
 const NUMBER_OF_ADS = 8;
 
+const types = {
+  flat: `Квартира`,
+  bungalow: `Бунгало`,
+  palace: `Дворец`,
+  house: `Дом`
+};
+
 const map = document.querySelector(`.map`);
-const mapPins = document.querySelector(`.map__pins`);
+const mapPins = map.querySelector(`.map__pins`);
+const filtersContainer = map.querySelector(`.map__filters-container`);
+
+// Используем шаблоны
 const pinTemplate = document.querySelector(`#pin`)
   .content
   .querySelector(`.map__pin`);
+
+const cardTemplate = document.querySelector(`#card`)
+  .content
+  .querySelector(`.map__card`);
 
 let rooms = Offer.MIN_ROOMS;
 
@@ -60,20 +94,11 @@ const getAdAuthor = function (i) {
 };
 
 
-// Значения для offer
-const getOfferPrice = function () {
-  return getRandomIntInclusive(Offer.MIN_PRICE, Offer.MAX_PRICE);
-};
-
 // Получаем рандомный элемент из массива
 const getRandomElement = function (elements) {
   return elements[getRandomIntInclusive(0, elements.length - 1)];
 };
 
-// Генерирует тип жилья
-const getOfferType = function () {
-  return getRandomElement(Offer.TYPE);
-};
 
 // Генерирует количество комнат
 const getOfferRooms = function (minElement, maxElement) {
@@ -86,20 +111,10 @@ const getOfferGuests = function () {
   return rooms * Offer.PERSON_PER_ROOM;
 };
 
-// Генерирует время заезда
-const getCheckin = function () {
-  return getRandomElement(Offer.CHECKIN);
-};
-
-// Генерирует время выезда
-const getCheckout = function () {
-  return getRandomElement(Offer.CHECKOUT);
-};
-
 
 // Генерирует координаты
 const getAdLocation = function () {
-  let adLocation = {
+  const adLocation = {
     x: getRandomIntInclusive(MapSize.MIN_WIDTH, MapSize.MAX_WIDTH),
     y: getRandomIntInclusive(MapSize.MIN_HEIGHT, MapSize.MAX_HEIGHT)
   };
@@ -117,16 +132,16 @@ const getRandomLengthArray = function (minimumQuantity, elements) {
 // Генерирует содержание предложения
 const getAdOffer = function (location) {
   let adOffer = {
-    title: `Заголовок предложения`,
+    title: getRandomElement(Offer.TITLE),
     address: location,
-    price: getOfferPrice(),
-    type: getOfferType(),
+    price: getRandomIntInclusive(Offer.MIN_PRICE, Offer.MAX_PRICE),
+    type: getRandomElement(Offer.TYPE),
     rooms: getOfferRooms(Offer.MIN_ROOMS, Offer.MAX_ROOMS),
     guests: getOfferGuests(),
-    checkin: getCheckin(),
-    checkout: getCheckout(),
+    checkin: getRandomElement(Offer.CHECKIN),
+    checkout: getRandomElement(Offer.CHECKOUT),
     features: getRandomLengthArray(Offer.MIN_FEATURES, Offer.FEATURES),
-    description: `Описание локации`,
+    description: getRandomElement(Offer.DESCRIPTION),
     photos: getRandomLengthArray(Offer.MIN_PHOTOS, Offer.PHOTOS)
   };
   return adOffer;
@@ -139,7 +154,7 @@ const getAds = function () {
 
   for (let i = 0; i < NUMBER_OF_ADS; i++) {
     const location = getAdLocation();
-    let similarAd = {
+    const similarAd = {
       author: getAdAuthor(i),
       offer: getAdOffer(location),
       location
@@ -150,7 +165,7 @@ const getAds = function () {
 };
 
 
-// Создаем объявление
+// Создаем пин объявления
 const renderAdOnMap = function (ad) {
   const pinElement = pinTemplate.cloneNode(true);
   const pinX = ad.location.x - Pin.WIDTH / 2;
@@ -162,6 +177,130 @@ const renderAdOnMap = function (ad) {
   avatarElement.alt = ad.offer.title;
 
   return pinElement;
+};
+
+
+// Удаляем потомков у элемента
+const removeChildren = function (element) {
+  while (element.firstChild) {
+    element.firstChild.remove();
+  }
+};
+
+
+// Создаем потомков из  массива
+const renderChildElements = function (
+    parentNode,
+    elements,
+    tagName,
+    basicClass,
+    startOfCompositeClass,
+    isImg
+) {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+    const childElement = document.createElement(tagName);
+
+    // Создается составной класс если есть
+    if (startOfCompositeClass) {
+      const сompositeClass = startOfCompositeClass + element;
+      childElement.classList.add(basicClass, сompositeClass);
+    } else {
+      childElement.classList.add(basicClass);
+    }
+
+    if (isImg) {
+      childElement.src = element;
+      childElement.width = 45;
+      childElement.height = 40;
+      childElement.alt = `Фотография жилья`;
+    }
+
+    fragment.appendChild(childElement);
+  }
+  parentNode.appendChild(fragment);
+};
+
+
+// Создаем преимущества у карточки
+const renderCardElements = function (
+    parentSelector,
+    childTagName,
+    card,
+    elementsArray,
+    basicClass,
+    startOfCompositeClass,
+    isImg
+) {
+  const parentNode = card.querySelector(parentSelector);
+
+  removeChildren(parentNode);
+  renderChildElements(
+      parentNode,
+      elementsArray,
+      childTagName,
+      basicClass,
+      startOfCompositeClass,
+      isImg
+  );
+};
+
+
+// Создаем карточку объявления
+const renderCard = function (ad) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardTitle = cardElement.querySelector(`.popup__title`);
+  const cardPrice = cardElement.querySelector(`.popup__text--price`);
+  const cardType = cardElement.querySelector(`.popup__type`);
+  const cardCapacity = cardElement.querySelector(`.popup__text--capacity`);
+  const cardTime = cardElement.querySelector(`.popup__text--time`);
+  const cardDescription = cardElement.querySelector(`.popup__description`);
+  const cardAvatar = cardElement.querySelector(`.popup__avatar`);
+
+  cardTitle.textContent = ad.offer.title;
+  cardPrice.textContent = `${ad.offer.price}₽/ночь`;
+  cardType.textContent = types[ad.offer.type];
+  cardCapacity.textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  cardTime.textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
+
+  // Cоздаем преимущества
+  renderCardElements(
+      `.popup__features`,
+      `li`,
+      cardElement,
+      ad.offer.features,
+      `popup__feature`,
+      `popup__feature--`,
+      false
+  );
+
+  cardDescription.textContent = ad.offer.description;
+
+  // Cоздаем фотографии
+  renderCardElements(
+      `.popup__photos`,
+      `img`,
+      cardElement,
+      ad.offer.photos,
+      `popup__photo`,
+      false,
+      true
+  );
+
+  cardAvatar.src = ad.author.avatar;
+
+  return cardElement;
+};
+
+
+// Добавляем все карточки на карту
+const renderCardOnMap = function (adsData) {
+  const fragment = document.createDocumentFragment();
+  fragment.appendChild(renderCard(adsData[0]));
+  map.insertBefore(fragment, filtersContainer);
 };
 
 
@@ -179,6 +318,8 @@ const renderAdsOnMap = function (adsData) {
 const ads = getAds();
 // Вызываем функцию создания объявлений на карте
 renderAdsOnMap(ads);
+// Вызываем функцию создания карточки
+renderCardOnMap(ads);
 
 
 map.classList.remove(`map--faded`);
