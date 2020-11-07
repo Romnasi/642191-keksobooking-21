6e77ff0cbc07fa3511f0aa9d72f763d8cb73118e
Roman Nasyrov <romnasi@gmail.com>
2020-11-08 02:34:00 +0300

    Рефакторит sync.js

--- js/sync.js
+++ js/sync.js
@@ -11,61 +11,71 @@
 
   const TIMEOUT_IN_MS = 1000;
 
-  // const request = function (data, onSucces, URL, method = Methods.GET) {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.responseType = `json`;
-  //
-  //   xhr.addEventListener(`load`, function () {
-  //     onSucces(xhr.response);
-  //   });
-  //
-  //   xhr.open(`POST`, URL);
-  //   xhr.send(data);
-  // };
-  //
-  //
-  // // Отправляем на сервер
-  // const sendData = function (data, onSucces) {
-  //   request(data, onSucces, sendURL);
-  // };
+  const Methods = {
+    GET: `GET`,
+    POST: `POST`
+  };
 
-  const sendData = function (data, onSucces) {
+
+  // Проверяем статус ответа сервера
+  const checkStatusCode = function (xhr, onSuccess, onError) {
+    if (xhr.status === statusCode.OK) {
+      onSuccess(xhr.response);
+    } else {
+      onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
+    }
+  };
+
+
+  // Обработчик на ошибку соединения
+  const checkConnectError = function (xhr, onError) {
+    xhr.addEventListener(`error`, function () {
+      onError(`Произошла ошибка соединения`);
+    });
+  };
+
+
+  // Обработчик на ошибку таймаута
+  const checkTimeoutError = function (xhr, onError) {
+    xhr.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+    });
+  };
+
+
+  const request = function (data, onSuccess, URL, method = Methods.GET, onError) {
     const xhr = new XMLHttpRequest();
     xhr.responseType = `json`;
 
     xhr.addEventListener(`load`, function () {
-      onSucces(xhr.response);
+      if (method === Methods.GET) {
+        checkStatusCode(xhr, onSuccess, onError);
+      } else {
+        onSuccess(xhr.response);
+      }
     });
 
-    xhr.open(`POST`, sendURL);
+    if (method === Methods.GET) {
+      xhr.timeout = TIMEOUT_IN_MS;
+
+      checkConnectError(xhr, onError);
+      checkTimeoutError(xhr, onError);
+    }
+
+    xhr.open(method, URL);
     xhr.send(data);
+  };
+
+
+  // Отправляем на сервер
+  const sendData = function (data, onSuccess) {
+    request(data, onSuccess, sendURL, Methods.POST);
   };
 
 
   // Загружаем с сервера
   const getData = function (onSuccess, onError) {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = `json`;
-
-    xhr.addEventListener(`load`, function () {
-      if (xhr.status === statusCode.OK) {
-        onSuccess(xhr.response);
-      } else {
-        onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener(`error`, function () {
-      onError(`Произошла ошибка соединения`);
-    });
-    xhr.addEventListener(`timeout`, function () {
-      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
-    });
-
-    xhr.timeout = TIMEOUT_IN_MS;
-
-    xhr.open(`GET`, getURL);
-    xhr.send();
+    request(null, onSuccess, getURL, Methods.GET, onError);
   };
 
 
