28a3037969cea1bb80e69890813431efa1652a73
Roman Nasyrov <romnasi@gmail.com>
2020-11-15 04:15:47 +0300

    Исправляет п.5.3 ТЗ -  Если в объекте с описанием объявления отсутствует поле offer, то метка объявления не должна отображаться на карте.

--- js/map.js
+++ js/map.js
@@ -19,33 +19,36 @@
 
 
   const renderPinOnMap = (ad) => {
-    const pinElement = pinTemplate.cloneNode(true);
-    const pinX = ad.location.x - Pin.WIDTH / 2;
-    const pinY = ad.location.y - Pin.HEIGHT;
-    pinElement.style.left = `${pinX}px`;
-    pinElement.style.top = `${pinY}px`;
-    const avatarElement = pinElement.querySelector(`img`);
-    avatarElement.src = ad.author.avatar;
-    avatarElement.alt = ad.offer.title;
+    // Если в объекте с описанием объявления отсутствует поле offer, то метка объявления не должна отображаться на карте.
+    if (ad.offer) {
+      const pinElement = pinTemplate.cloneNode(true);
+      const pinX = ad.location.x - Pin.WIDTH / 2;
+      const pinY = ad.location.y - Pin.HEIGHT;
+      pinElement.style.left = `${pinX}px`;
+      pinElement.style.top = `${pinY}px`;
+      const avatarElement = pinElement.querySelector(`img`);
+      avatarElement.src = ad.author.avatar;
+      avatarElement.alt = ad.offer.title;
 
-    const showCard = () => {
-      window.remove.removeCard();
-      window.map.renderCardOnMap(ad);
-    };
+      const showCard = () => {
+        window.remove.removeCard();
+        window.map.renderCardOnMap(ad);
+      };
 
-    const onPinElementClick = () => {
-      showCard();
-    };
+      const onPinElementClick = () => {
+        showCard();
+      };
 
-    const onPinElementPress = (evt) => {
-      window.util.isEnterEvent(evt, showCard);
-    };
+      const onPinElementPress = (evt) => {
+        window.util.isEnterEvent(evt, showCard);
+      };
 
-    pinElement.addEventListener(`click`, onPinElementClick);
+      pinElement.addEventListener(`click`, onPinElementClick);
 
-    pinElement.addEventListener(`keydown`, onPinElementPress);
+      pinElement.addEventListener(`keydown`, onPinElementPress);
 
-    return pinElement;
+      return pinElement;
+    }
   };
 
   // Добавляем карточку на карту
